938a50b47798893fd8388de5946ede1575e4f6b7
kayoz <dawid.kaj09@gmail.com>
2025-03-30 14:14:57 +0200

    database + init method

---
+++ app/database_module.py
@@ -0,0 +1,71 @@
+
+from sqlalchemy import Float, Integer
+import sqlalchemy as db
+from sqlalchemy.orm import declarative_base
+
+db_base = declarative_base()
+
+# tables
+class TrackDb(db_base):
+    __tablename__ = 'tracks'
+    # data is like below
+    # Song,Artist,Streams,Daily Streams,Genre,Release Year,Peak Position,Weeks on Chart,Lyrics Sentiment,TikTok Virality,Danceability,Acousticness,Energy
+    # Track 14728,EchoSync,689815326,796199,Trap,2021,81,8,0.2,17,0.11,0.59,0.6
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    song = db.Column(db.String(255), nullable=False)
+    artist = db.Column(db.String(255), nullable=False)
+    streams = db.Column(db.BigInteger, nullable=False)
+    daily_streams = db.Column(db.Integer, nullable=False)
+    genre = db.Column(db.String(100), nullable=False)
+    release_year = db.Column(db.Integer, nullable=False)
+    peak_position = db.Column(db.Integer, nullable=False)
+    weeks_on_chart = db.Column(db.Integer, nullable=False)
+    lyrics_sentiment = db.Column(db.Float, nullable=False)
+    tiktok_virality = db.Column(db.Integer, nullable=False)
+    danceability = db.Column(db.Float, nullable=False)
+    acousticness = db.Column(db.Float, nullable=False)
+    energy = db.Column(db.Float, nullable=False)
+
+def importTracksFromKaggle(engine, csv_file_path):
+    import csv
+    
+    Session = db.orm.sessionmaker(bind=engine)
+    session = Session()
+    with open(csv_file_path, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        next(csv_reader)  # Skip the header row
+        for row in csv_reader:
+            # insert data
+            session.add(TrackDb(
+                song = row[0],
+                artist = row[1],
+                streams = row[2],
+                daily_streams = row[3],
+                genre = row[4],
+                release_year = row[5],
+                peak_position = row[6],
+                weeks_on_chart = row[7],
+                lyrics_sentiment = row[8],
+                tiktok_virality = row[9],
+                danceability = row[10],
+                acousticness = row[11],
+                energy = row[12],
+            ))
+    
+    # save
+    session.commit()
+    session.close()
+
+def get_engine(username : str, password : str, host : str, port : int, database : str):
+    return db.create_engine(
+        url = f'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'
+    )
+    
+def create_tables(engine, db_base = db_base):
+    db_base.metadata.create_all(engine)   
+    print("Tables created.")
+    
+def check_and_create_tables(engine):
+    inspector = db.inspect(engine)
+    if 'tracks' not in inspector.get_table_names():
+        create_tables(engine)
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-import json
+import os
+# import json
+# from requests import HTTPError
 from flask import render_template, request, redirect, url_for
 from app import app
-from requests import HTTPError
 from app import config_module as conf
-
+from app import database_module as db
 
 @app.route('/')
 def index():    
@@ -16,16 +17,50 @@ def index():
         )
     
 @app.route('/init')
-def test():
-    config = conf.loadConfig()    
+def init():
+    try:
+        config = conf.loadConfig()
     
-    return render_template('test.html', 
-            now=datetime.now(),
-            app_name = config['GENERAL']['app_name'],
+        db_engine = db.get_engine(
+            username = config['DATABASE']['username'],
+            password = config['DATABASE']['password'],
+            host     = config['DATABASE']['host'],
+            port     = config['DATABASE']['port'],
+            database = config['DATABASE']['database']
         )
+        db.check_and_create_tables(db_engine)
+        
+        # current dir
+        directory = os.path.dirname((os.path.abspath(__file__)))
+        # get to the parent of current dir
+        directory = os.path.dirname(directory)
+        
+        # get to child dir
+        directory = os.path.join(directory, 'datasrc')
+        
+        filepath = os.path.join(directory, 'music_dataset.csv')
+        
+        print(filepath)
+        
+        
+        db.importTracksFromKaggle(db_engine,filepath)
+        
+        # return render_template('test.html', 
+        #         now=datetime.now(),
+        #         app_name = config['GENERAL']['app_name'],
+        #     )
+        return "<h2>Operation successfull!</h2>"
+    
+    
+    except Exception as e:
+        print(f"ERROR. '{type(e)}' : '{str(e)}'")
+        return "<h2>Operation failed!</h2><p>Check console logs</p>"
+        
+    
+    
     
 @app.route('/objects')
-def index():    
+def objects():    
     config = conf.loadConfig()
     
     return render_template('index.html', 
