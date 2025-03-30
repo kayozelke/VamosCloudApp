91f7474692440ee008222eac82f9bf39dab37aa3
kayoz <dawid.kaj09@gmail.com>
2025-03-30 18:55:08 +0200

    custom ports and debug mode

--- webserver.py
+++ webserver.py
@@ -1,6 +1,17 @@
+import os
 from app import app
 
-if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 80)))
-    # app.run()
+import argparse
+
+parser = argparse.ArgumentParser(description='Run the Flask application.')
+parser.add_argument('-p','--port', type=int, default=80, help='Port to run the application on.')
+parser.add_argument('-d','--debug', action='store_true', help='Enable debug mode.')
+args = parser.parse_args()
+
+# -------------------- Start Flask server --------------------
+if __name__ == '__main__':
+    if args.debug:
+        app.run(debug=True, port=int(os.environ.get('PORT', args.port)))
+    else:
+        app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', args.port)))
+
