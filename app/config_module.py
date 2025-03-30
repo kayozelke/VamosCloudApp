1393da7107a23feea20aba14eda34bf745dce41c
kayoz <dawid.kaj09@gmail.com>
2025-03-30 13:55:14 +0200

    update 3 files and create 1 file

---
+++ app/config_module.py
@@ -0,0 +1,36 @@
+import configparser
+import os
+
+DEFAULT_CONF = {
+    'GENERAL' : {
+        'app_name' : 'Vamos Cloud App'
+    },
+    'DATABASE' : {
+        'username' : 'admin',
+        'password' : 'admin',
+        'host'     : 'localhost',
+        'port'     : '3306',
+        'database' : 'vamos_cloud_app'
+    }
+}
+
+def writeConfigIfNotExists(config_path = 'config.ini'):
+    if not os.path.exists(config_path):
+        config = configparser.ConfigParser()
+        config.read_dict(DEFAULT_CONF)
+        with open(config_path, 'w') as configfile:
+            config.write(configfile)
+            configfile.close()
+            return True
+    else:
+        return False
+        
+
+
+def loadConfig(config_path = 'config.ini', createIfNotExists = True):
+    if createIfNotExists:
+        writeConfigIfNotExists(config_path=config_path)
+    config = configparser.ConfigParser()
+    config.read(config_path)
+    
+    return {s:dict(config.items(s)) for s in config.sections()}
--- app/routes.py
+++ app/routes.py
@@ -3,25 +3,43 @@ import json
 from flask import render_template, request, redirect, url_for
 from app import app
 from requests import HTTPError
+from app import config_module as conf
 
 
 @app.route('/')
-def index():
+def index():    
+    config = conf.loadConfig()
+    
     return render_template('index.html', 
-            now=datetime.now()
+            now=datetime.now(),
+            app_name = config['GENERAL']['app_name'],
         )
     
-@app.route('/test')
+@app.route('/init')
 def test():
+    config = conf.loadConfig()    
+    
     return render_template('test.html', 
-            now=datetime.now()
+            now=datetime.now(),
+            app_name = config['GENERAL']['app_name'],
+        )
+    
+@app.route('/objects')
+def index():    
+    config = conf.loadConfig()
+    
+    return render_template('index.html', 
+            now=datetime.now(),
+            app_name = config['GENERAL']['app_name'],
         )
 
 
 @app.route('/add', methods=['POST'])
 def add():
+    config = conf.loadConfig()
     input_text = request.form.get('input_field')
     return render_template('index.html', 
-        now=datetime.now()
+        now=datetime.now(),
+        app_name = config['GENERAL']['app_name'],
     )
 
